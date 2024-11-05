ac836114bf55c4492996622d1a954bad960cc32b
Your Name <you@example.com>
2024-11-05 08:44:42 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Link} from "react-router-dom"
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
+
+
+const App = () => {
+
+    return (
+    <>
+ <div id="container">
+      
+      <div id="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/" element=<Home/>/>
+          <Route path="/blue" element= <Blue/>/>
+          <Route path="/red" element=<Red/>/>
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+};
+
+export default App
---
+++ src/components/Red.jsx
@@ -0,0 +1,10 @@
+const Red = () => {
+  return (
+    <>
+      <div className="red">
+        <h1>RED</h1>
+      </div>
+    </>
+  )
+}
+export default Red
